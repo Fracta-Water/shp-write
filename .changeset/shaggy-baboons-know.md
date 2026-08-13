5cdad0d21eb40a12f393a02a61c38d8f0eef37b1
Jin Igarashi <jigarashi@fracta.ai>
2026-08-13 09:39:46 +0900

    fix: updated index.d.ts to declare @fracta-ai/shp-write instead of old @mapbox namespace

---
+++ .changeset/shaggy-baboons-know.md
@@ -0,0 +1,6 @@
+---
+"@fracta-ai/shp-write": patch
+---
+
+fix: updated index.d.ts to declare @fracta-ai/shp-write instead of old @mapbox namespace
+  
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-declare module "@mapbox/shp-write" {
+declare module "@fracta-ai/shp-write" {
   export type OGCGeometry =
     'NULL' |
     'POINT' |
